bfb08b9303fad4fa9338fc0c548fef0c2a606db1
tHaItRuOnG123 <91905960+tHaItRuOnG123@users.noreply.github.com>
2024-02-22 18:43:59 +0700

    create improok-care

---
+++ react-improok-care/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+import Home from "./components/Home/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-improok-care/src/components/Login/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+    return (
+        <>
+            <div>
+                <h1>Đăng nhập</h1>
+            </div>
+        </>
+    )
+}
+
+export default Login;
