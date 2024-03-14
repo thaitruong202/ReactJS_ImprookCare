31dc5b41a4b8fee64c6f4d16620544e3edf20719
Trần Đăng Tuấn <82080825+TuanTran0168@users.noreply.github.com>
2024-03-14 18:18:43 +0700

    fix DoctorMenu

--- react-improok-care/src/App.js
+++ react-improok-care/src/App.js
@@ -1,22 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'react-toastify/dist/ReactToastify.css';
-import cookie from "react-cookies"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.css";
+import cookie from "react-cookies";
 import { createContext, useReducer } from "react";
 import UserReducer from "./reducers/UserReducer";
 import PrescriptionReducer from "./reducers/PrescriptionReducer";
-import Login from "./components/Login/Login"
-import Register from "./components/Register/Register"
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import Home from "./components/Home/Home";
 import Header from "./layout/Header/Header";
 import Footer from "./layout/Footer/Footer";
-import ForgetPassword from "./components/ForgetPassword/ForgetPassword"
-import Collaboration from "./components/Collaboration/Collaboration"
-import Personal from "./components/Personal/Personal"
+import ForgetPassword from "./components/ForgetPassword/ForgetPassword";
+import Collaboration from "./components/Collaboration/Collaboration";
+import Personal from "./components/Personal/Personal";
 import Profile from "./components/Profile/Profile";
 import { ToastContainer } from "react-toastify";
 import Admin from "./components/Admin/Admin";
-import ChangePassword from "./components/ChangePassword/ChangePassword"
+import ChangePassword from "./components/ChangePassword/ChangePassword";
 import PhoneVerification from "./components/PhoneVerification/PhoneVerification";
 import Appointment from "./components/Appointment/Appointment";
 import History from "./components/History/History";
@@ -26,19 +26,23 @@ import BookingDoctor from "./components/Booking/BookingDoctor";
 import ProfileDoctor from "./components/Doctor/ProfileDoctor";
 import BookingDetail from "./components/Booking/BookingDetail";
 import DoctorMessage from "./components/Doctor/DoctorMessage";
-import Message from "./components/Message/Message"
+import Message from "./components/Message/Message";
 import PaymentResult from "./components/Payment/PaymentResult";
 import BookingManagement from "./components/Doctor/BookingManagement";
 import Prescription from "./components/Doctor/Prescription";
 import Schedule from "./components/Doctor/Schedule";
+import VideoCall from "./components/VideoCall/Videocall";
 
 export const UserContext = createContext();
 export const BookingManagementContext = createContext();
 export const MyPrescriptionContext = createContext();
 
 const App = () => {
-  const [user, dispatch] = useReducer(UserReducer, cookie.load("user") || null)
-  const [booking, dispatchBooking] = useReducer(PrescriptionReducer, cookie.load("bookingInfo") || null)
+  const [user, dispatch] = useReducer(UserReducer, cookie.load("user") || null);
+  const [booking, dispatchBooking] = useReducer(
+    PrescriptionReducer,
+    cookie.load("bookingInfo") || null
+  );
   // const [prescriptionCounter, prescriptionDispatch] = useReducer(MyPrescriptionCounterReducer, 0)
   return (
     <UserContext.Provider value={[user, dispatch]}>
@@ -46,29 +50,35 @@ const App = () => {
         <BrowserRouter basename="/improok">
           <Header />
           <Routes>
-            <Route exact path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/forgetpassword' element={<ForgetPassword />} />
-            <Route path='/collaboration' element={<Collaboration />} />
-            <Route path='/personal' element={<Personal />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/admin' element={<Admin />} />
-            <Route path='/changepassword' element={<ChangePassword />} />
-            <Route path='/phoneverification' element={<PhoneVerification />} />
-            <Route path='/appointment' element={<Appointment />} />
-            <Route path='/history' element={<History />} />
-            <Route path='/doctor' element={<Doctor />} />
-            <Route path='/profiledoctor' element={<ProfileDoctor />} />
-            <Route path='/booking' element={<Booking />} />
-            <Route path='/doctor/:profileDoctorId' element={<BookingDoctor />} />
-            <Route path='/booking/doctor/:profileDoctorId' element={<BookingDetail />} />
-            <Route path='/doctormessage' element={<DoctorMessage />} />
-            <Route path='/message' element={<Message />} />
-            <Route path='/paymentresult' element={<PaymentResult />} />
-            <Route path='/bookingmanagement' element={<BookingManagement />} />
-            <Route path='/prescription' element={<Prescription />} />
-            <Route path='/schedule' element={<Schedule />} />
+            <Route exact path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgetpassword" element={<ForgetPassword />} />
+            <Route path="/collaboration" element={<Collaboration />} />
+            <Route path="/personal" element={<Personal />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/changepassword" element={<ChangePassword />} />
+            <Route path="/phoneverification" element={<PhoneVerification />} />
+            <Route path="/appointment" element={<Appointment />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/doctor" element={<Doctor />} />
+            <Route path="/profiledoctor" element={<ProfileDoctor />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route
+              path="/doctor/:profileDoctorId"
+              element={<BookingDoctor />}
+            />
+            <Route
+              path="/booking/doctor/:profileDoctorId"
+              element={<BookingDetail />}
+            />
+            <Route path="/doctormessage" element={<DoctorMessage />} />
+            <Route path="/message" element={<Message />} />
+            <Route path="/paymentresult" element={<PaymentResult />} />
+            <Route path="/bookingmanagement" element={<BookingManagement />} />
+            <Route path="/prescription" element={<Prescription />} />
+            <Route path="/schedule" element={<Schedule />} />
           </Routes>
           <Footer />
           <ToastContainer />
@@ -76,6 +86,6 @@ const App = () => {
       </BookingManagementContext.Provider>
     </UserContext.Provider>
   );
-}
+};
 
 export default App;
--- react-improok-care/src/layout/DoctorLayout/DoctorMenu.js
+++ react-improok-care/src/layout/DoctorLayout/DoctorMenu.js
@@ -2,106 +2,121 @@ import { useContext, useEffect, useState } from "react";
 import { FaCalendarCheck, FaInfoCircle } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
 import { MdEditCalendar, MdLogout, MdMessage } from "react-icons/md";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, NavLink } from "react-router-dom";
 import { UserContext } from "../../App";
-import "./DoctorMenu.css"
+import "./DoctorMenu.css";
 
 const DoctorMenu = () => {
-    const nav = useNavigate();
-    const [, dispatch] = useContext(UserContext);
-    const [selectedItem, setSelectedItem] = useState(null);
-    // const handleItemClick = (item) => {
-    //     setSelectedItem(item);
-    // };
+  const nav = useNavigate();
+  const [, dispatch] = useContext(UserContext);
+  const [selectedItem, setSelectedItem] = useState(null);
+  // const handleItemClick = (item) => {
+  //     setSelectedItem(item);
+  // };
 
-    const handleItemClick = (item, path) => {
-        setSelectedItem(item);
-        nav(path);
-    };
+  const handleItemClick = (item, path) => {
+    setSelectedItem(item);
+    nav(path);
+  };
 
-    const logout = () => {
-        dispatch({
-            "type": "logout"
-        })
-        nav("/")
-    }
+  const logout = () => {
+    dispatch({
+      type: "logout",
+    });
+    nav("/");
+  };
 
-    return (
-        // <ul className="DoctorMenu">
-        //     <li><FaInfoCircle /><Link to="/doctor">Thông tin cá nhân</Link></li>
-        //     <li><MdEditCalendar /><Link to="/schedule">Đăng ký lịch khám</Link></li>
-        //     <li><FaCalendarCheck /><Link to="/bookingmanagement">Lịch hẹn</Link></li>
-        //     <li><ImProfile /><Link to="/profiledoctor">Hồ sơ</Link></li>
-        //     <li><MdMessage /><Link to="/doctormessage">Tin nhắn</Link></li>
-        //     <li onClick={logout}><MdLogout />Đăng xuất</li>
-        // </ul>
-        // <ul className="DoctorMenu">
-        //     <li
-        //         className={selectedItem === "info" ? "selected" : ""}
-        //         onClick={() => handleItemClick("info")}
-        //     >
-        //         <FaInfoCircle /><Link className={selectedItem === "info" ? "selectedText" : ""} onClick={() => handleItemClick("info")} to="/doctor">Thông tin cá nhân</Link>
-        //     </li>
-        //     <li
-        //         className={selectedItem === "schedule" ? "selected" : ""}
-        //         onClick={() => handleItemClick("schedule")}
-        //     >
-        //         <MdEditCalendar /><Link className={selectedItem === "schedule" ? "selectedText" : ""} onClick={() => handleItemClick("schedule")} to="/schedule">Đăng ký lịch khám</Link>
-        //     </li>
-        //     <li
-        //         className={selectedItem === "bookingmanagement" ? "selected" : ""}
-        //         onClick={() => handleItemClick("bookingmanagement")}
-        //     >
-        //         <FaCalendarCheck /><Link className={selectedItem === "bookingmanagement" ? "selectedText" : ""} onClick={() => handleItemClick("bookingmanagement")} to="/bookingmanagement">Lịch hẹn</Link>
-        //     </li>
-        //     <li
-        //         className={selectedItem === "profiledoctor" ? "selected" : ""}
-        //         onClick={() => handleItemClick("profiledoctor")}
-        //     >
-        //         <ImProfile /><Link className={selectedItem === "profiledoctor" ? "selectedText" : ""} onClick={() => handleItemClick("profiledoctor")} to="/profiledoctor">Hồ sơ</Link>
-        //     </li>
-        //     <li className={selectedItem === "message" ? "selected" : ""}
-        //         onClick={() => handleItemClick("message")}>
-        //         <MdMessage /><Link className={selectedItem === "message" ? "selectedText" : ""} onClick={() => handleItemClick("message")} to="/doctormessage">Tin nhắn</Link>
-        //     </li>
-        //     <li onClick={logout}>
-        //         <MdLogout />Đăng xuất
-        //     </li>
-        // </ul>
-        <div className="DoctorMenu">
-            <div
-                className={selectedItem === "doctor" ? "selected" : ""}
-                onClick={() => handleItemClick("doctor", "/doctor")}
-            >
-                <FaInfoCircle /><div>Thông tin cá nhân</div>
-            </div>
-            <div
-                className={selectedItem === "schedule" ? "selected" : ""}
-                onClick={() => handleItemClick("schedule", "/schedule")}
-            >
-                <MdEditCalendar /><div>Đăng ký lịch khám</div>
-            </div>
-            <div
-                className={selectedItem === "bookingmanagement" ? "selected" : ""}
-                onClick={() => handleItemClick("bookingmanagement", "/bookingmanagement")}
-            >
-                <FaCalendarCheck /><div>Lịch hẹn</div>
-            </div>
-            <div
-                className={selectedItem === "profiledoctor" ? "selected" : ""}
-                onClick={() => handleItemClick("profiledoctor", "/profiledoctor")}
-            >
-                <ImProfile /><div>Hồ sơ</div>
-            </div>
-            <div className={selectedItem === "message" ? "selected" : ""}
-                onClick={() => handleItemClick("message", "/doctormessage")}>
-                <MdMessage /><div>Tin nhắn</div>
-            </div>
-            <div onClick={logout}>
-                <MdLogout />Đăng xuất
-            </div>
-        </div>
-    )
-}
+  return (
+    // <ul className="DoctorMenu">
+    //     <li><FaInfoCircle /><Link to="/doctor">Thông tin cá nhân</Link></li>
+    //     <li><MdEditCalendar /><Link to="/schedule">Đăng ký lịch khám</Link></li>
+    //     <li><FaCalendarCheck /><Link to="/bookingmanagement">Lịch hẹn</Link></li>
+    //     <li><ImProfile /><Link to="/profiledoctor">Hồ sơ</Link></li>
+    //     <li>activeCMdMessage activeto="/doctormessage">Tin nhắn</Link></li>
+    //     <li onClick={logout}><MdLogout />Đăng xuất</li>
+    // </ul>
+    // <ul className="DoctorMenu">
+    //     <li
+    //         activeClassName={activeItem === "info" ? "selected" : ""}
+    //         onClick={() => handleItemClick("info")}
+    //     >activeC    //    activeInfoCircle /><Link className={selectedItem === "info" ? "selectedText" : ""} onClick={() => handleItemClick("info")} to="/doctor">Thông tin cá nhân</Link>
+    //     </li>
+    //     <li
+    //         activeClassName={activeItem === "schedule" ? "selected" : ""}
+    //         onClick={() => handleItemClick("schedule")}
+    //     >activeC    //    activeEditCalendar /><Link className={selectedItem === "schedule" ? "selectedText" : ""} onClick={() => handleItemClick("schedule")} to="/schedule">Đăng ký lịch khám</Link>
+    //     </li>
+    //     <li
+    //         activeClassName={activeItem === "bookingmanagement" ? "selected" : ""}
+    //         onClick={() => handleItemClick("bookingmanagement")}
+    //     >activeC    //    activeCalendarCheck /><Link className={selectedItem === "bookingmanagement" ? "selectedText" : ""} onClick={() => handleItemClick("bookingmanagement")} to="/bookingmanagement">Lịch hẹn</Link>
+    //     </li>
+    //     <li
+    //         activeClassName={activeItem === "profiledoctor" ? "selected" : ""}
+    //         onClick={() => handleItemClick("profiledoctor")}
+    //     >activeC    //    activeProfile /><Link className={selectedItem === "profiledoctor" ? "selectedText" : ""} onClick={() => handleItemClick("profiledoctor")} to="/profiledoctor">Hồ sơ</Link>
+    //     </li>
+    //     <li activeClassName={activeItem === "message" ? "selected" : ""}
+    //         onClick={() => handleItemClick("message")}>activeC    //    activeMessage /><Link className={selectedItem === "message" ? "selectedText" : ""} onClick={() => handleItemClick("message")} to="/doctormessage">Tin nhắn</Link>
+    //     </liactiveC
+    //   activeClick={logout}>
+    //         <MdLogout />Đăng xuất
+    //     </li>
+    // </ul>
+    <div className="DoctorMenu">
+      <NavLink
+        // className={selectedItem === "doctor" ? "selected" : ""}
+        activeClassName="active"
+        onClick={() => handleItemClick("doctor", "/doctor")}
+        to="/doctor"
+      >
+        <FaInfoCircle />
+        {/* <div>Thông tin cá nhân</div> */}
+        Thông tin cá nhân
+      </NavLink>
+      <NavLink
+        // className={selectedItem === "schedule" ? "selected" : ""}
+        activeClassName="active"
+        onClick={() => handleItemClick("schedule", "/schedule")}
+        to="/schedule"
+      >
+        <MdEditCalendar />
+        {/* <div>Đăng ký lịch khám</div> */}
+        Đăng ký lịch khám
+      </NavLink>
+      <NavLink
+        // className={selectedItem === "bookingmanagement" ? "selected" : ""}
+        activeClassName="active"
+        onClick={() =>
+          handleItemClick("bookingmanagement", "/bookingmanagement")
+        }
+        to="/bookingmanagement"
+      >
+        <FaCalendarCheck />
+        <div>Lịch hẹn</div>
+      </NavLink>
+      <NavLink
+        // className={selectedItem === "profiledoctor" ? "selected" : ""}
+        activeClassName="active"
+        onClick={() => handleItemClick("profiledoctor", "/profiledoctor")}
+        to="/profiledoctor"
+      >
+        <ImProfile />
+        <div>Hồ sơ</div>
+      </NavLink>
+      {/* <div
+        activeClassName={activeItem === "message" ? "selected" : ""}
+        onClick={() => handleItemClick("message", "/doctormessage")}
+      >
+        <MdMessage />
+        <div>Tin nhắn</div>
+      </divactiveC
+      <diactivek={logout}>
+        <MdLogout />
+        Đăng xuất
+      </div> */}
+    </div>
+  );
+};
 
 export default DoctorMenu;
